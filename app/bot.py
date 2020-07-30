577f8371f8b5034c6d3967efa6a621595a36dfbe
Asjid Kalam <asjid.kalam@gmail.com>
2020-07-30 22:16:49 +0530

    langs fixed

--- app/bot.py
+++ app/bot.py
@@ -10,30 +10,23 @@ def getBounty(status):
     dateToday = str(datetime.today().strftime('%Y-%m-%d'))
     resp = []
 
-<<<<<<< HEAD
     def getLanguages(obj, name):
         lang = str(obj['CodebasePrimaryLanguage'])
         if(lang == name):
             bounty = str(obj['PackageName'] + " : " + obj['CodebasePrimaryLanguage'])
             resp.append(bounty)
 
-=======
->>>>>>> e8b073960410e55df5dbcf9ccc0bd70e38d8ec17
     response = requests.get('https://files.huntr.dev/index.json')
     response.raise_for_status()
     jsonResponse = response.json()
     for bounties in range(len(jsonResponse)):
         obj = jsonResponse[bounties]
         if(status == "new"):
-<<<<<<< HEAD
             # TODO: new bounties - diff
-=======
->>>>>>> e8b073960410e55df5dbcf9ccc0bd70e38d8ec17
             date = str(obj['DisclosureDate'])
             if(date == dateToday):
                 bounty = str(obj['PackageName'] + " : " + obj['CodebasePrimaryLanguage'])
                 resp.append(bounty)
-<<<<<<< HEAD
         elif(status == "python"):
             getLanguages(obj, "Python")
         elif(status == "js"):
@@ -42,8 +35,6 @@ def getBounty(status):
             getLanguages(obj, "Ruby")
         elif(status == "php"):
             getLanguages(obj, "PHP")
-=======
->>>>>>> e8b073960410e55df5dbcf9ccc0bd70e38d8ec17
         else:
             bounty = str(obj['PackageName'] + " : " + obj['CodebasePrimaryLanguage'])
             resp.append(bounty)       
@@ -59,7 +50,6 @@ def bot():
     resp = MessagingResponse()
     msg = resp.message()
     responded = False
-<<<<<<< HEAD
     langs = ['python', 'js', 'ruby', 'php', 'javascript']
 
     if incoming_msg in langs:
@@ -69,8 +59,6 @@ def bot():
             msg.body(new_bounties[i])
         responded = True
 
-=======
->>>>>>> e8b073960410e55df5dbcf9ccc0bd70e38d8ec17
     if 'new' in incoming_msg:
         # check for new bounties
         new_bounties = getBounty('new')
@@ -80,23 +68,14 @@ def bot():
             for i in range(len(new_bounties)):
                 msg.body(new_bounties[i])
         responded = True
-<<<<<<< HEAD
 
-=======
->>>>>>> e8b073960410e55df5dbcf9ccc0bd70e38d8ec17
     if 'all' in incoming_msg:
         # return all the bounties
         new_bounties = getBounty('all')
         for i in range(len(new_bounties)):
             msg.body(new_bounties[i])
-<<<<<<< HEAD
         responded = True
         
-=======
-            msg.body("\n")
-        responded = True
-
->>>>>>> e8b073960410e55df5dbcf9ccc0bd70e38d8ec17
     if not responded:
         msg.body('Invalid Command!')
     return str(resp)
